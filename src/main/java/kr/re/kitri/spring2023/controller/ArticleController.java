40b78e766c79ce079c1b48e7687e4626d03cd59f
pyojihye <pyojihye95@gmail.com>
2023-07-10 17:05:36 +0900

    Article - p.35

--- src/main/java/kr/re/kitri/spring2023/controller/ArticleController.java
+++ src/main/java/kr/re/kitri/spring2023/controller/ArticleController.java
@@ -2,7 +2,7 @@ package kr.re.kitri.spring2023.controller;
 
 import kr.re.kitri.spring2023.service.ArticleService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
